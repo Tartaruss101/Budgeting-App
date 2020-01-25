b381e1e0dc9bf5650d56bda7dce3890538cf850d
Tartaruss101 <russell.brown83@gmail.com>
2020-01-25 13:17:11 -0600

    This is the body of the application. Needless to say, there is a lot to do.

---
+++ BudgetApp.py
@@ -0,0 +1,20 @@
+import calendar
+import os
+from html.parser import HTMLParser
+
+file_path = os.getcwd() + "\\Python Projects\\Budget App\\calendar.html"
+
+Calendar_String = calendar.HTMLCalendar(calendar.SUNDAY).formatyear(2020, 4)
+
+#HELLO
+
+month_calendar = open(file_path, "w")
+month_calendar.write(Calendar_String)
+month_calendar.close()
+
+
+calender_parser = HTMLParser()
+calender_parser.feed(Calendar_String)
+calender_parser.close()
+
+quit()
