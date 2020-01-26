6a24a3cb6b6cd8d7da315adefd5661299826d1d2
Tartaruss101 <russell.brown83@gmail.com>
2020-01-26 15:59:50 -0600

    Added Implementation TODO's

--- BudgetApp.py
+++ BudgetApp.py
@@ -2,9 +2,10 @@ import calendar
 import os
 from html.parser import HTMLParser
 
+#Test Functions
 file_path = os.getcwd() + "\\Python Projects\\Budget App\\calendar.html"
 
-Calendar_String = calendar.HTMLCalendar(calendar.SUNDAY).formatyear(2020, 4)
+Calendar_String = calendar.HTMLCalendar(calendar.SUNDAY).formatyear( 2020, 4 )
 
 
 
@@ -16,5 +17,12 @@ month_calendar.close()
 calender_parser = HTMLParser()
 calender_parser.feed(Calendar_String)
 calender_parser.close()
+#end Test Functions
+
+
+# TODO: implement database
+# TODO: implement Data Collection
+# TODO: implement calendar generation
+# TODO: implement display
 
 quit()
